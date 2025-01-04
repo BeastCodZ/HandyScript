2c6bdef332bc2b73cef6a3a453c85961c33f1603
BeastCodZ <62057520+BeastCodZ@users.noreply.github.com>
2025-01-04 13:52:43 +0530

    seperated ms office and windows

--- script.ps1
+++ script.ps1
@@ -4,13 +4,24 @@ $global:Tasks = @(
     @{ Name = "Install SpotX"; Selected = $false; Action = { iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify" } },
     @{ Name = "Install VSC & Github"; Selected = $false; Action = { winget install -e --id Microsoft.VisualStudioCode; winget install -e --id GitHub.GitHubDesktop } }
 @{
-    Name = "Activate Microsoft Windows 10 | 11 & MS OFFICE"; 
+    Name = "Activate Microsoft Windows 10 | 11)"; 
     Selected = $false; 
     Action = { 
-            $cmdUrl = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/Separate-Files-Version/Activators/Online_KMS_Activation.cmd';
+            $cmdUrl = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/Separate-Files-Version/Activators/Online_KMS_Activation.cmd /K-Windows';
             $tempFile = "$env:TEMP\Online_KMS_Activation.cmd";
             Invoke-RestMethod -Uri $cmdUrl -OutFile $tempFile;
-            Start-Process -FilePath "cmd.exe" -ArgumentList "/c $tempFile" -Wait;
+            Start-Process -FilePath "cmd.exe" -ArgumentList "/c $tempFile /K-Windows" -Wait;
+            Remove-Item -Path $tempFile -Force;
+    }
+}
+@{
+    Name = "Activate MS OFFICE"; 
+    Selected = $false; 
+    Action = { 
+            $cmdUrl = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/Separate-Files-Version/Activators/Online_KMS_Activation.cmd /K-Office';
+            $tempFile = "$env:TEMP\Online_KMS_Activation.cmd";
+            Invoke-RestMethod -Uri $cmdUrl -OutFile $tempFile;
+            Start-Process -FilePath "cmd.exe" -ArgumentList "/c $tempFile /K-Office" -Wait;
             Remove-Item -Path $tempFile -Force;
     }
 }
