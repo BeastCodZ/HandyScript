f1f4c8817efc99010a2552f85c557e8c3b9665f9
BeastCodZ <62057520+BeastCodZ@users.noreply.github.com>
2025-01-04 14:03:53 +0530

    removed exit when not admin

--- script.ps1
+++ script.ps1
@@ -1,7 +1,6 @@
 # Check if the script is running with admin privileges
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "This script must be run as an administrator." -ForegroundColor Red
-    exit
 }
 
 $global:Tasks = @(
